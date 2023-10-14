b3025ccedfc0cf5aaceeb68bcf4fb46c9aff9542
whypxralich <whypxrlch@gmail.com>
2023-10-14 18:19:35 +0300

    Create input-math.py

---
+++ input-math.py
@@ -0,0 +1,6 @@
+print ("Hello! Write me two numbers and I will multiply them!")
+num1 = int(input())
+print ("Nice. Now whrite me second number.")
+num2 = int(input())
+answer = num1 + num2
+print ("Here is you answer:", answer)
